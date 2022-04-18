45f36b0feb32c722008e8aaeceb7125440098c1a
shamexln <shamexln@hotmail.com>
2022-04-18 12:22:49 +0800

    revise code to active top windows, refer to https://www.cnblogs.com/ziwuge/archive/2012/01/06/2315342.html

--- goweb/capture/capture.go
+++ goweb/capture/capture.go
@@ -22,11 +22,18 @@ type RECT struct {
 }
 
 var (
-	user32, _              = syscall.LoadLibrary("user32.dll")
-	findWindowW, _         = syscall.GetProcAddress(user32, "FindWindowW")
-	getWindowRect, _       = syscall.GetProcAddress(user32, "GetWindowRect")
-	setWindowPos, _        = syscall.GetProcAddress(user32, "SetWindowPos")
-	setForegroundWindow, _ = syscall.GetProcAddress(user32, "SetForegroundWindow")
+	user32, _                   = syscall.LoadLibrary("user32.dll")
+	findWindowW, _              = syscall.GetProcAddress(user32, "FindWindowW")
+	getWindowRect, _            = syscall.GetProcAddress(user32, "GetWindowRect")
+	setWindowPos, _             = syscall.GetProcAddress(user32, "SetWindowPos")
+	setForegroundWindow, _      = syscall.GetProcAddress(user32, "SetForegroundWindow")
+	getForegroundWindow, _      = syscall.GetProcAddress(user32, "GetForegroundWindow")
+	getWindowThreadProcessId, _ = syscall.GetProcAddress(user32, "GetWindowThreadProcessId")
+	attachThreadInput, _        = syscall.GetProcAddress(user32, "AttachThreadInput")
+	showWindow, _               = syscall.GetProcAddress(user32, "ShowWindow")
+
+	kernel32, _           = syscall.LoadLibrary("Kernel32.dll")
+	getCurrentThreadId, _ = syscall.GetProcAddress(kernel32, "GetCurrentThreadId")
 )
 
 //HWND WINAPI FindWindow(
@@ -56,21 +63,19 @@ func GetWindowDimensions(hwnd HWND) *RECT {
 	return &rect
 }
 
-func SetWindowPos(hwnd HWND, cx int32, cy int32) HWND {
-	//dd := 1
+func SetWindowPos(hwnd HWND, cx int32, cy int32, hWndInsertAfter HWND) HWND {
 
 	ret, _, err := syscall.SyscallN(
 		setWindowPos,
 		uintptr(hwnd),
-		//uintptr(unsafe.Pointer(&dd)),
-		uintptr(0),
+		uintptr(hWndInsertAfter),
 		uintptr(0),
 		uintptr(0),
 		uintptr(cx),
 		uintptr(cy),
 		uintptr(0x0040),
 	)
-	fmt.Printf("Return: %v\n", err)
+	fmt.Printf("SetWindowPos: %v\n", err)
 	return HWND(ret)
 }
 
@@ -85,19 +90,86 @@ func SetForegroundWindow(hwnd HWND) bool {
 	return val != 0
 }
 
+func GetForegroundWindow() HWND {
+	ret, _, err := syscall.SyscallN(
+		getForegroundWindow,
+		0,
+		0,
+		0,
+	)
+	fmt.Printf("GetForegroundWindow: %v\n", err)
+	fmt.Printf("GetForegroundWindow: %v\n", ret)
+	return HWND(ret)
+}
+
+func GetCurrentThreadId() int64 {
+	ret, _, err := syscall.SyscallN(
+		getCurrentThreadId,
+		0,
+	)
+	fmt.Printf("GetCurrentThreadId: %v\n", err)
+	fmt.Printf("GetCurrentThreadId: %v\n", ret)
+	return int64(ret)
+}
+
+func GetWindowThreadProcessId(hwnd HWND) int64 {
+	var processId int64
+	ret, _, err := syscall.SyscallN(
+		getWindowThreadProcessId,
+		uintptr(hwnd),
+		uintptr(unsafe.Pointer(&processId)),
+	)
+	fmt.Printf("GetWindowThreadProcessId: %v\n", ret)
+	fmt.Printf("GetWindowThreadProcessId: %v\n", err)
+
+	return int64(ret)
+}
+
+func AttachThreadInput(curThreadId int64, curProcessId int64, attach int) bool {
+	//var bVal bool = true
+	ret, _, err := syscall.SyscallN(
+		attachThreadInput,
+		uintptr(curThreadId),
+		uintptr(curProcessId),
+		uintptr(attach),
+	)
+
+	fmt.Printf("AttachThreadInput: %v\n", err)
+	val := (uint)(ret)
+	return val != 0
+}
+
+func ShowWindow(hwnd HWND) bool {
+	ret, _, err := syscall.SyscallN(
+		showWindow,
+		uintptr(hwnd),
+		uintptr(5),
+	)
+	fmt.Printf("ShowWindow: %v\n", err)
+	val := (uint)(ret)
+	return val != 0
+}
+
 func (c Capture) CaptureImage(imgFileName string) {
 	defer syscall.FreeLibrary(user32)
 
 	hwnd := FindWindowByTitle("DraegerEIT")
-	//hwnd := FindWindowByTitle("计算器")
 	fmt.Printf("Return: %d\n", hwnd)
+
+	curHwnd := GetForegroundWindow()
+	curID := GetCurrentThreadId()
+	curForeID := GetWindowThreadProcessId(curHwnd)
+	AttachThreadInput(curID, curForeID, 1)
+	ShowWindow(hwnd)
+
 	var rect *RECT
 	if hwnd > 0 {
 		rect = GetWindowDimensions(hwnd)
-
+		dd := -1
+		SetWindowPos(hwnd, rect.Right-rect.Left, rect.Bottom-rect.Top, HWND(dd))
 		rtnv := SetForegroundWindow(hwnd)
-		fmt.Printf("Return: %v\n", rtnv)
-		SetWindowPos(hwnd, rect.Right-rect.Left, rect.Bottom-rect.Top)
+		fmt.Printf("SetForegroundWindow: %v\n", rtnv)
+		//SetWindowPos(hwnd, rect.Right-rect.Left, rect.Bottom-rect.Top)
 
 	}
 
@@ -143,6 +215,15 @@ func (c Capture) CaptureImage(imgFileName string) {
 		png.Encode(file, img)
 
 		fmt.Printf("#%d : %v \"%s\"\n", i, bounds, newpath)
+	}
+
+	// reset previous active window
+	if curHwnd > 0 {
+
+		dd := -2
+		SetWindowPos(hwnd, rect.Right-rect.Left, rect.Bottom-rect.Top, HWND(dd))
+		AttachThreadInput(curID, curForeID, 0)
+		SetForegroundWindow(curHwnd)
 	}
 }
 
